01cb7523cafcdb08b112f6ae624264ff17f2a324
Per-Olof <per.olof.svensson@hotmail.com>
2016-06-08 13:14:26 +0200

    Removed some references to Structuremap Version 1.0.2

--- Geta.SEO.Sitemaps/Properties/AssemblyInfo.cs
+++ Geta.SEO.Sitemaps/Properties/AssemblyInfo.cs
@@ -2,16 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Geta.SEO.Sitemaps")]
+[assembly: AssemblyTitle("Geta.SEO.Sitemaps.Knowit")]
 [assembly: AssemblyDescription("Search Engine Sitemap generator for EPiServer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Geta")]
+[assembly: AssemblyCompany("Knowit")]
 [assembly: AssemblyProduct("Geta.SEO.Sitemaps")]
 [assembly: AssemblyCopyright("Copyright © Geta 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9f3a4ec0-97a5-47d5-91b2-3e60843d0ff1")]
-[assembly: AssemblyVersion("1.5.3.0")]
-[assembly: AssemblyInformationalVersion("1.5.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 [assembly: InternalsVisibleTo("Geta.SEO.Sitemaps.Tests")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
